69bbba133e7aca0d0eb2cb22dffbe13e5ccfa876
ijagberg <ijagberg@gmail.com>
2024-09-30 10:59:51 +0200

    add extremely basic string interpolation (WIP as fuck)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -7,18 +7,26 @@ use reqwest::Method;
 #[derive(clap::Parser)]
 struct Args {
     request_name: String,
+    #[arg(short, default_value = "Requests")]
+    dir: String,
+    #[arg(short)]
+    env: Option<String>,
 }
 
 fn main() -> Result<(), KuiperError> {
     let args = Args::parse();
 
-    let requests = kuiper_lib::Requests::evaluate("requests")?;
+    let requests = kuiper_lib::Requests::evaluate(&args.dir, args.env)?;
+    // println!("found the following requests:");
+    // for (name, _) in &requests {
+    //     println!("{}", name);
+    // }
 
     if let Some(request) = requests.get(&args.request_name) {
-        println!("{:#?}", request);
+        // println!("{:#?}", request);
         send_request(request);
     } else {
-        eprintln!("could not find request with name '{}'", args.request_name);
+        return Err(KuiperError::RequestNotFound(args.request_name));
     }
 
     Ok(())
@@ -28,7 +36,12 @@ fn send_request(req: &Request) {
     let client = reqwest::blocking::Client::new();
     let mut request = client.request(Method::from_str(req.method()).unwrap(), req.uri());
     for (name, value) in req.headers() {
-        request = request.header(name, value);
+        if let Some(v) = value {
+            request = request.header(name, v);
+        }
+    }
+    if let Some(body) = req.body() {
+        request = request.json(body);
     }
     let request = request.build().unwrap();
 
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -10,26 +10,36 @@ use std::{
 };
 
 pub struct Requests {
-    root: String,
+    _root: String,
     requests: HashMap<String, Request>,
 }
 
 impl Requests {
-    pub fn evaluate(root: &str) -> KuiperResult<Self> {
-        let map = evaluate_dir(root.into(), Headers::new(), Env::new())?;
-        let stripped_map = map
+    pub fn evaluate(root: &str, env: Option<String>) -> KuiperResult<Self> {
+        let environment = if let Some(env) = env {
+            read_env(root.into(), env.as_str())?
+        } else {
+            Env::new()
+        };
+        let map = evaluate_dir(root.into(), Headers::new())?;
+        let mut stripped_map: HashMap<String, Request> = map
             .into_iter()
             .map(|(key, value)| {
-                if let Some(key) = key.strip_prefix(root) {
-                    (key.to_owned(), value)
-                } else {
-                    (key, value)
-                }
+                let without_root = key.strip_prefix(root).unwrap_or(&key);
+                let without_slash = without_root.strip_prefix('/').unwrap_or(&without_root);
+                let without_ext = without_slash
+                    .strip_suffix(".kuiper")
+                    .unwrap_or(without_slash);
+                (without_ext.to_owned(), value)
             })
             .collect();
 
+        for (_, request) in stripped_map.iter_mut() {
+            request.interpolate(&environment)?;
+        }
+
         Ok(Requests {
-            root: root.to_string(),
+            _root: root.to_string(),
             requests: stripped_map,
         })
     }
@@ -39,7 +49,25 @@ impl Requests {
     }
 }
 
-pub type Headers = HashMap<String, String>;
+impl IntoIterator for Requests {
+    type Item = <HashMap<String, Request> as IntoIterator>::Item;
+    type IntoIter = <HashMap<String, Request> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.requests.into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a Requests {
+    type Item = (&'a String, &'a Request);
+    type IntoIter = std::collections::hash_map::Iter<'a, String, Request>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.requests.iter()
+    }
+}
+
+pub type Headers = HashMap<String, Option<String>>;
 pub type Env = HashMap<String, String>;
 pub type KuiperResult<T> = Result<T, KuiperError>;
 
@@ -49,11 +77,11 @@ pub struct Request {
     headers: Headers,
     params: Value,
     method: String,
-    body: Option<Vec<u8>>,
+    body: Option<Value>,
 }
 
 impl Request {
-    fn add_header_if_not_exists(&mut self, header_name: String, header_value: String) {
+    fn add_header_if_not_exists(&mut self, header_name: String, header_value: Option<String>) {
         if let Entry::Vacant(vacant_entry) = self.headers.entry(header_name) {
             vacant_entry.insert(header_value);
         }
@@ -70,13 +98,45 @@ impl Request {
     pub fn headers(&self) -> &Headers {
         &self.headers
     }
+
+    fn interpolate(&mut self, env: &Env) -> KuiperResult<()> {
+        let new_url = interpolate(&self.uri, env)?;
+        for (_, value) in self.headers.iter_mut() {
+            if let Some(v) = value {
+                let new_value = interpolate(&v.clone(), env)?;
+                *v = new_value;
+            }
+        }
+        self.uri = new_url;
+
+        if let Some(body) = &self.body {
+            let s = body.to_string();
+            let new_body_s = interpolate(&s, env)?;
+            self.body = serde_json::from_str(&new_body_s)?;
+        }
+        // TODO: params, body
+
+        Ok(())
+    }
+    
+    pub fn body(&self) -> Option<&Value> {
+        self.body.as_ref()
+    }
 }
 
-fn evaluate_dir(
-    path: PathBuf,
-    mut headers: Headers,
-    _env: Env,
-) -> KuiperResult<HashMap<String, Request>> {
+fn read_env(dir: PathBuf, env: &str) -> KuiperResult<Env> {
+    let file_contents = std::fs::read_to_string(dir.join(format!("{}.env", env)))?;
+
+    let mut env = Env::new();
+    for line in file_contents.lines() {
+        let (key, value) = line.split_once('=').ok_or(KuiperError::FileFormatError)?;
+        env.insert(key.to_owned(), value.to_owned());
+    }
+
+    Ok(env)
+}
+
+fn evaluate_dir(path: PathBuf, mut headers: Headers) -> KuiperResult<HashMap<String, Request>> {
     // look for a header file in the dir
     read_headers(path.join("headers.json"), &mut headers)?;
 
@@ -86,12 +146,11 @@ fn evaluate_dir(
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            for (name, value) in evaluate_dir(path, headers.clone(), _env.clone())? {
+            for (name, value) in evaluate_dir(path, headers.clone())? {
                 requests.insert(name, value);
             }
         } else if let Some(ext) = path.extension() {
             if ext == "kuiper" {
-                println!("found request: {:?}", path);
                 let file = File::open(&path)?;
                 let reader = BufReader::new(file);
 
@@ -112,22 +171,55 @@ fn evaluate_dir(
 }
 
 fn read_headers(path: PathBuf, headers: &mut Headers) -> KuiperResult<()> {
-    if let Ok(header_file_contents) = fs::read_to_string(path) {
-        // missing header file simply means we dont add any headers
-        let file_headers: Headers = serde_json::from_str(&header_file_contents)?;
+    match File::open(&path) {
+        Ok(file) => {
+            let reader = BufReader::new(file);
 
-        for (name, value) in file_headers {
-            // TODO: handle interpolation
-            headers.insert(name.to_owned(), value.to_owned());
+            let file_headers: Headers = serde_json::from_reader(reader)?;
+            for (name, value) in file_headers {
+                // TODO: handle interpolation
+                headers.insert(name.to_owned(), value.to_owned());
+            }
         }
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => return Ok(()),
+            _ => return Err(KuiperError::IoError(e)),
+        },
     }
     Ok(())
+}
+
+fn interpolate(input: &str, env: &Env) -> KuiperResult<String> {
+    let mut result = input.to_owned();
+    for (start_idx, _) in input.match_indices("${{") {
+        let (end_idx, _) = input[start_idx..]
+            .match_indices("}}")
+            .next()
+            .ok_or(KuiperError::InterpolationError)?;
+        let interpolated_name = &input[start_idx + 3..start_idx + end_idx];
+        let interpolated_value = env
+            .get(interpolated_name)
+            .ok_or(KuiperError::InterpolationError)?;
+        // println!(
+        //     "in '{}', found interpolation '{}', replacing with value '{}'",
+        //     input, interpolated_name, interpolated_value
+        // );
+        result = result.replace(
+            &input[start_idx..start_idx + end_idx + 2],
+            &interpolated_value,
+        );
+    }
+
+    Ok(result)
 }
 
 #[derive(Debug)]
 pub enum KuiperError {
     IoError(std::io::Error),
     JsonError(serde_json::Error),
+    RequestNotFound(String),
+    InterpolationError,
+    FileFormatError,
 }
 
 impl Error for KuiperError {}
@@ -140,6 +232,9 @@ impl Display for KuiperError {
             match self {
                 KuiperError::IoError(error) => format!("I/O error: {error}"),
                 KuiperError::JsonError(error) => format!("JSON error: {error}"),
+                KuiperError::RequestNotFound(name) => format!("request not found: {name}"),
+                KuiperError::InterpolationError => format!("interpolation error"),
+                KuiperError::FileFormatError => format!("file format error"),
             }
         )
     }
@@ -165,7 +260,7 @@ mod tests {
     fn evaluate_test_dir() {
         let dir = "../requests";
 
-        let requests = Requests::evaluate(dir.into()).unwrap();
+        let requests = Requests::evaluate(dir.into(), None).unwrap();
 
         println!(
             "{}",
@@ -173,53 +268,70 @@ mod tests {
         );
 
         {
-            // `request_in_root.kuiper` should have headers from `requests/`
+            // `request_in_root.kuiper`
+            // root_header_1 from `requests/headers.json`
+            // root_header_2 from `requests/headers.json`
+            // root_header_3 from `requests/headers.json`,
+            //     with value `null` because it is overwritten by `request_in_root.kuiper`
 
-            let request_in_root = requests.get("request_in_root.kuiper").unwrap();
+            let request_in_root = requests.get("request_in_root").unwrap();
             assert_eq!(
                 request_in_root.headers().len(),
-                2,
-                "there are two headers in request_in_root.kuiper"
+                3,
+                "there should be 3 headers in request_in_root.kuiper"
             );
             assert_eq!(
-                request_in_root.headers().get("root_header_1").unwrap(),
+                request_in_root.headers()["root_header_1"].as_ref().unwrap(),
                 "root_value_1"
             );
             assert_eq!(
-                request_in_root.headers().get("root_header_2").unwrap(),
+                request_in_root.headers()["root_header_2"].as_ref().unwrap(),
                 "root_value_2"
             );
+            assert_eq!(request_in_root.headers()["root_header_3"], None);
         }
 
         {
             // `request_in_subdir.kuiper`
             // root_header_1 from `requests/headers.json`
             // root_header_2 from `requests/headers.json`, overwritten by `requests/subdir/headers.json`
+            // root_header_3 from `requests/headers.json`
             // subdir_header_1 from `requests/subdir/headers.json`
             // request_specific_header_1 from `requests/subdir/request_in_subdir.kuiper`
 
-            let request_in_subdir = requests.get("request_in_subdir.kuiper").unwrap();
+            let request_in_subdir = requests.get("subdir/request_in_subdir").unwrap();
             assert_eq!(
                 request_in_subdir.headers().len(),
-                4,
-                "there are four headers in request_in_subdir.kuiper"
+                5,
+                "there should be 5 headers in request_in_subdir.kuiper"
             );
             assert_eq!(
-                request_in_subdir.headers().get("root_header_1").unwrap(),
+                request_in_subdir.headers()["root_header_1"]
+                    .as_ref()
+                    .unwrap(),
                 "root_value_1"
             );
             assert_eq!(
-                request_in_subdir.headers().get("root_header_2").unwrap(),
+                request_in_subdir.headers()["root_header_2"]
+                    .as_ref()
+                    .unwrap(),
                 "subdir_value_2"
             );
             assert_eq!(
-                request_in_subdir.headers().get("subdir_header_1").unwrap(),
+                request_in_subdir.headers()["root_header_3"]
+                    .as_ref()
+                    .unwrap(),
+                "root_value_3"
+            );
+            assert_eq!(
+                request_in_subdir.headers()["subdir_header_1"]
+                    .as_ref()
+                    .unwrap(),
                 "subdir_value_1"
             );
             assert_eq!(
-                request_in_subdir
-                    .headers()
-                    .get("request_specific_header_1")
+                request_in_subdir.headers()["request_specific_header_1"]
+                    .as_ref()
                     .unwrap(),
                 "request_specific_header_value_1"
             );
